5678287228283114061f32421ffd0d679a2c8273
Mathew Topper <damm_horse@yahoo.co.uk>
2024-04-18 10:46:44 +0100

    Specify download name for octet streams

--- src/lib/components/mdsvex/elements/a.svelte
+++ src/lib/components/mdsvex/elements/a.svelte
@@ -1,7 +1,16 @@
-<script>
-  export let href;
+<script lang="ts">
+  export let href: string;
+  let data: HTMLAnchorElement;
+  $: getDownload = () => {
+    if (!href.startsWith("data:application/octet-stream")) return;
+    return data?.innerText;
+  };
 </script>
 
-<a class="text-blue-600 visited:text-purple-600 hover:underline" {href}>
+<a
+  class="text-blue-600 visited:text-purple-600 hover:underline"
+  {href}
+  bind:this={data}
+  download={getDownload()}>
   <slot />
 </a>
